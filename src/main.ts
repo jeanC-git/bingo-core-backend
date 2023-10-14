230047ead4dedb3d786f12c537dc231999c35aad
Jean Carlo <jeanca2198@gmail.com>
2023-10-14 15:39:13 -0500

    set env port

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,9 @@ async function bootstrap() {
         })
     );
 
-    await app.listen(3000);
+    // await app.listen(3000);
+    await app.listen(process.env.PORT, "0.0.0.0");
+
 }
 
 bootstrap();
